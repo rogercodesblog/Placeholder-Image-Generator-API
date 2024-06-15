dc77c7f7f5f4eb01c397b64ebd5f03d9a5a132d7
RogerCodes <rogelioherreradev@gmail.com>
2024-06-15 10:46:19 -0600

    Removed authorization from program.cs

--- Placeholder-Image-Generator-API/Program.cs
+++ Placeholder-Image-Generator-API/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.ResponseCompression;
 using Placeholder_Image_Generator_API.Services.PlaceholderImageService;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System.IO.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,19 +21,12 @@ builder.Services.Configure<GzipCompressionProviderOptions>(options =>
     options.Level = CompressionLevel.Fastest;
 });
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
